3f9bc26eb29a01d2819fbf838d785cb3fb9f3038
zh-star <1731866148@qq.com>
2019-09-07 10:32:37 +0800

    练习

---
+++ Untitled-1.java
@@ -0,0 +1,7 @@
+public class Test{       
+     public static void main(String[] args){              
+           System.out.println("Hello,Java");      
+             System.out.println("My name is:"+args[0]);   
+             
+      } 
+}
