fec6413be9e6db4be946796c6937e8141e2839cc
GitHubzhxx <1731866148@qq.com>
2019-09-14 21:57:50 +0800

    提交git

--- 20190914/Text.java
+++ 20190914/Text.java
@@ -5,10 +5,24 @@ public class Text {
         int sum = 0;//求和的结果
         int temp = 1;
         int i = 1;
-        for (i = 1; i <= num; i++) {
-            temp = temp * i;//结果是num的阶乘
-            sum = sum + temp;
-        }
+        int j = 0;
+        //方法一：
+        // for (i = 1; i <= num; i++) {
+        //     temp = temp * i;//结果是num的阶乘
+        //     sum = sum + temp;
+        // }
+
+        //方法二：
+        //阶乘和  将每次求得值相加
+        // for ( j = 1; j <= num; j++) {
+        //     //分别求每个数的阶乘   1！；2！；3！；4！；5！
+        //     temp = 1;//每次求阶乘前，都要让temp==1
+        //     for (i = 1; i <= j; i++) {
+        //         temp *= i;
+        //     }
+        //     sum = sum + temp;
+        // }
+
         System.out.println(sum);
     }
 }
