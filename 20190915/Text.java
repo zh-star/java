e9849fd155229e9ebc0b2895adbb190aa178a6b4
GitHubzhxx <1731866148@qq.com>
2019-09-15 22:44:05 +0800

    提交git

--- 20190915/Text.java
+++ 20190915/Text.java
@@ -1,27 +1,42 @@
 public class Text {
     //4.2 实现代码: 递归求 N 的阶乘  
+    // public static void main(String[] args) {
+    //     int ret = factor(5);//递归
+    //     int ret1 = factor1(5);//非递归
+    //     System.out.println("ret = " + ret);
+    //     System.out.println("ret1 = " + ret1);
+    // }
+    // //递归求阶乘
+    // public static int factor(int num) {
+    //     if (num == 1) {
+    //         return 1;
+    //     }
+    //     return num * factor(num - 1);
+    // }
+
+
+
+    // //非递归求阶乘
+    // public static int factor1(int num) {
+    //     int temp = 1;
+    //     for(int i = 1; i <= num; i++) {
+    //          temp *= i;
+    //     }
+    //     return temp;
+    // }
+
+
+
+    //4.3. 实现代码: 递归求 1 + 2 + 3 + ... + 10
     public static void main(String[] args) {
-        int ret = factor(5);//递归
-        int ret1 = factor1(5);//非递归
-        System.out.println("ret = " + ret);
-        System.out.println("ret1 = " + ret1);
+        int ret = sum(10);
+        System.out.println(ret);
     }
-    //递归求阶乘
-    public static int factor(int num) {
+    public static int sum(int num) {
         if (num == 1) {
             return 1;
         }
-        return num * factor(num - 1);
+        return num + sum(num - 1);
     }
-
-
-
-    //非递归求阶乘
-    public static int factor1(int num) {
-        int temp = 1;
-        for(int i = 1; i <= num; i++) {
-             temp *= i;
-        }
-        return temp;
-    }
+    
 }
