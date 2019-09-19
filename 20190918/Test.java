753d334c9ae4da3496c923203307df47e36dd32b
GitHubzhxx <1731866148@qq.com>
2019-09-19 12:43:40 +0800

    提交git

--- 20190918/Test.java
+++ 20190918/Test.java
@@ -27,21 +27,63 @@ public class Test {
     //}
 
     //打印数组内容（使用方法）
+    // public static void main(String[] args) {
+    //     int[] arr = new int[] {1,2,3,4,5};
+    //     int[] output = transForm(arr);
+    //     printArray(arr);
+    //     printArray(output);
+    // }
+    // public static void printArray(int[] a) {
+    //     for(int i = 0; i < a.length; i++) {
+    //         System.out.println(a[i]);
+    //     }
+    // }
+    // //写一个方法, 将数组中的每个元素都 * 2
+    // public static int[] transForm(int[] a) {
+    //     //因为数组是引用类型，实参给形参传值时 传的是地址
+    //     //若要不修改原有的数组，则要重新创建一个新的数组，并将原数组赋值给它
+    //     int[] ret = new int[a.length];
+    //     for(int i = 0; i < a.length; i++) {
+    //         ret[i] = a [i] * 2;//不改变原有值
+    //     }
+    //     return ret;
+    // }
+
+
+    //实现一个自己版本的数组转字符串
+    // public static void main(String[] args) {
+    //     int[] arr = {1,2,3,4,5};
+    //     String ret = ToString(arr);
+    //     System.out.println(ret);
+
+    // }
+    // public static String ToString(int[] a) {
+    //     //打印结果应该是[1,2,3,4,5]
+    //     String ret = "[";
+    //     for(int i = 0; i < a.length; i++) {
+    //         ret += a[i];
+    //         //每次打印完一个数时在之后加上“，”
+    //         if(i != a.length - 1) {
+    //             ret += ",";
+    //         }
+    //     }
+    //     ret += "]";
+    //     return ret;
+    // }
+
+
+
+    //数组的拷贝函数
     public static void main(String[] args) {
-        int[] arr = new int[] {1,2,3,4,5};
-        transForm(arr);
-        printArray(arr);
+        int[] arr = {1,2,3,4,5};
+        int[] ret = CopyOf(arr);
+        System.out.println(Arrays.toString(ret));
     }
-    public static void printArray(int[] a) {
-        for(int i = 0; i < a.length; i++) {
-            System.out.println(a[i]);
+    public static int[] CopyOf(int[] a) {
+        int[] ret = new int[a.length];
+        for(int i = 0; i <= a.length; i++) {
+            ret[i] = a[i];
         }
+        return ret;
     }
-    public static void transForm(int[] a) {
-        for(int i = 0; i < a.length; i++) {
-            a[i] = a [i] * 2;
-        }
-    }
-
-
 }
