fb2e83a888f7b2f3252be83c2ee39d3eadfc6d77
GitHubzhxx <1731866148@qq.com>
2019-09-26 23:08:51 +0800

    提交git

--- 20190925/src/LinkedList.java
+++ 20190925/src/LinkedList.java
@@ -7,6 +7,7 @@ class LinkedNode {
     }
 }
 public class LinkedList {
+
     //创建一个头节点（第一个节点），有了这个节点就可以根据next找到剩下的所有元素
     private LinkedNode head = null;
 
@@ -53,4 +54,92 @@ public class LinkedList {
         System.out.println("]");
         return;
     }
+
+
+    //任意位置插入节点
+    public void addIndex(int index,int elem){
+        LinkedNode node = new LinkedNode(elem);
+        //先对index进行合法性校验，需要计算链表的长度
+        int len = size();
+        if(index < 0 || index > len) {
+            return;
+        }
+        if(index == 0){
+            //头插
+            addFist(elem);
+            return;
+        }
+        if(index == len){
+            //尾插
+            addLast(elem);
+        }
+        //链表中间插入
+        //prv找到index - 1对应的位置
+        LinkedNode prv = getIndexPos(index - 1);
+        node.next = prv.next;
+        prv.next = node;
+    }
+
+
+    //求链表的长度
+    private int size(){
+        int size = 0;
+        for(LinkedNode cur = this.head; cur != null; cur = cur.next){
+            size ++;
+        }
+        return size;
+    }
+    //找到链表中index的前一个位置，并返回index前一个位置的引用
+    private LinkedNode getIndexPos(int index){
+        LinkedNode cur = this.head;
+        for(int i = 0; i < index; i++){
+            cur = cur.next;
+        }
+        return cur;
+    }
+
+
+    //查找是否包含关键字key是否在单链表当中
+    public boolean contains(int toFind){
+        for(LinkedNode cur = this.head; cur != null; cur = cur.next){
+            if(cur.date == toFind){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //删除第一次出现关键字为elem的节点
+    public void remove(int toRemove){
+        //LinkedNode cur = new LinkedNode(elem);
+        //先要判断链表是否为空
+        if(this.head == null){
+            return;
+        }
+        //删除节点为第一个节点
+        if(this.head.date == toRemove){
+            this.head = this.head.next;
+        }
+        //删除中间元素或最后一个元素
+        LinkedNode prv = searchPrv(toRemove);
+        prv.next = prv.next.next;
+    }
+
+    private LinkedNode searchPrv(int toRemove){
+        //空链表校验
+        if(this.head == null){
+            return null;
+        }
+        LinkedNode cur = this.head;
+        while(cur.next != null) {
+            if(cur.next.date == toRemove)
+            {
+                return cur;
+            }
+            cur = cur.next;
+        }
+        return null;
+    }
+
+
 }
--- 20190925/src/Test.java
+++ 20190925/src/Test.java
@@ -2,6 +2,9 @@ public class Test {
     public static void main(String[] args){
         testAddFist();
         testAddLast();
+        testAddIndex();
+        testContans();
+        testRemove();
     }
 
     public static void testAddFist(){
@@ -23,6 +26,44 @@ public class Test {
         node.addLast(2);
         node.addLast(3);
         node.addLast(4);
+        node.display();
+    }
+
+    public  static void testAddIndex(){
+        System.out.println("任意位置插入节点");
+        LinkedList node = new LinkedList();
+        node.addLast(1);
+        node.addLast(2);
+        node.addLast(3);
+        node.addLast(4);
+        node.display();
+        node.addIndex(0,5);
+        node.display();
+    }
+
+    public static void testContans(){
+        //测试某个节点是否存在链表中
+        System.out.println("是否存在于链表中");
+        LinkedList node = new LinkedList();
+        //boolean ret1= node.contains(3);
+        //System.out.println(ret1);
+        node.addLast(1);
+        node.addLast(2);
+        node.addLast(3);
+        node.addLast(4);
+        boolean ret2 = node.contains(4);
+        System.out.println(ret2);
+    }
+
+    public static void testRemove(){
+        //测试删除元素
+        System.out.println("删除链表中的元素");
+        LinkedList node = new LinkedList();
+        node.addLast(1);
+        node.addLast(2);
+        node.addLast(3);
+        node.addLast(4);
+        node.remove(4);
         node.display();
     }
 }
