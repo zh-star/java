2dd810a4878ba72ec513e69fc144ea81dec18b99
GitHubzhxx <1731866148@qq.com>
2019-09-28 20:43:37 +0800

    提交git

--- 20190923/src/Person.java
+++ 20190923/src/Person.java
@@ -3,6 +3,10 @@ public class Person {
     private String sex;
     private int age;
 
+    //注： 1.一个类必须有一个构造函数，若程序员自己没有创建，系统会默认自己创建，
+    //       若创建了，系统不会自己在创建
+    //    2.构造函数的函数名必须和类名保持一致，没有返回值
+    //    3.构造函数支持重载（函数的参数 类型、个数 不同）
     public Person(String name) {
         this.name = name;
     }
@@ -18,6 +22,7 @@ public class Person {
     }
 
     public void Show(){
-        System.out.println("姓名:" + this.name + " 性别:" + this.sex + " 年龄:" + this.age);
+        System.out.println("姓名:" + this.name + " 性别:" +
+                this.sex + " 年龄:" + this.age);
     }
 }
