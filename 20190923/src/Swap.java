47d540f4d923c56ef9d81fdba9da52dfcb6c5a71
GitHubzhxx <1731866148@qq.com>
2019-09-28 20:43:27 +0800

    提交git

--- 20190923/src/Swap.java
+++ 20190923/src/Swap.java
@@ -1,10 +1,19 @@
-public class Swap {
-    private int a;
-    private int b;
+class Num {
+    int num;
 
-    public void Swap(){
-        int temp = a;
+    public Num(int num) {
+        this.num = num;
+    }
+}
+
+public class Swap {
+    public void Swap(int n,int m){
+        Num a = new Num(n);
+        Num b = new Num(m);
+        Num temp = a;
         a = b;
         b = temp;
+        System.out.println(a.num);
+        System.out.println(b.num);
     }
 }
