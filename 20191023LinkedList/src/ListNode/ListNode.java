cf9c8711b7bd82cf8c818c57d982e0fa802ebf3b
GitHubzhxx <1731866148@qq.com>
2019-11-04 23:30:38 +0800

    git

---
+++ 20191023LinkedList/src/ListNode/ListNode.java
@@ -0,0 +1,21 @@
+package ListNode;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: STAR
+ * Date: 2019 -11
+ * Time: 22:20
+ */
+class Node {
+    private int date;
+    private Node next;
+
+    public Node(int date) {
+        this.date = date;
+    }
+
+}
+public class ListNode {
+
+}
