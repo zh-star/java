86c59cde74b6609e3f516ba9cfd41121d163dd9e
GitHubzhxx <1731866148@qq.com>
2019-11-05 18:59:49 +0800

    git

---
+++ 20191115/src/TestDemo1.java
@@ -0,0 +1,62 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: STAR
+ * Date: 2019 -11
+ * Time: 9:52
+ */
+public class TestDemo1 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+        String str = scanner.next();
+        str = reverse(str,0,size - 1);
+        str = reverse(str,size,str.length() -1);
+        str = reverse(str,0,str.length() -1);
+        System.out.println(str);
+    }
+    public static String reverse(String str,int start,int end){
+        char[] ch = str.toCharArray();
+        while(start < end) {
+            char temp = ch[start];
+            ch[start] = ch[end];
+            ch[end] = temp;
+            start ++;
+            end --;
+        }
+        return  new String(ch);
+    }
+    public static String reverse(String str) {
+        char[] ch = str.toCharArray();
+        int left = 0;
+        int right = ch.length - 1;
+        while(left < right) {
+            char temp = ch[left];
+            ch[left] = ch[right];
+            ch[right] = temp;
+            left ++;
+            right --;
+        }
+        return  new String(ch);
+
+    }
+    public static void main2(String[] args){
+        String s = "abcdef";
+        String ret = reverse(s);
+        System.out.println(ret);
+    }
+    public static void main1(String[] args){
+        String str = "123456a";
+        char[] ch = str.toCharArray();
+        for (int i = 0; i < ch.length; i++) {
+            if(ch[i] < '0' && ch[i] >'9') {
+                System.out.println("不是数字字符串");
+                return;
+            }
+        }
+        System.out.println("是数字字符串");
+    }
+}
---
+++ 20191115/src/TestDemo2.java
@@ -0,0 +1,56 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: STAR
+ * Date: 2019 -11
+ * Time: 15:53
+ */
+public class TestDemo2 {
+    public static void main(String[] args){
+        //字符串的创建
+        String str11 = "abcdef";
+//        String str2 = new String("abcdef");
+//        char[] array = {'a','b','c','d'};
+//        int len = array.length;
+//        String str3 = new String(array);
+//        String str4 = new String();
+//        System.out.println(str4);
+//        System.out.println(str3);
+//        int len = str1.length();
+
+//        String str6 = "ab";
+//        System.out.println(Integer.toHexString(str6.hashCode()));
+//        String str7 = "cdef";
+//        System.out.println(Integer.toHexString(str7.hashCode()));
+//        String str8 = str6+str7;
+//        System.out.println(Integer.toHexString(str8.hashCode()));
+//        System.out.println(str11 == str8);// false
+//        System.out.println(str11.equals(str8));
+//
+//        String str1 = "abcdef";
+//        String str2 = "abfdef";
+//        System.out.println(str1.equals(str2));
+//        System.out.println(str1.equalsIgnoreCase(str2));
+        /**
+         * 如果对应位置相同一直往后走
+         * 找到不一样的字符 进行比较
+         * str1和str2
+         * str1 > str2  >0
+         * str1 < str2  <0
+         * str1 == str2 =0
+         */
+ //       System.out.println(str1.compareTo(str2));
+//        System.out.println(Integer.toHexString(str11.hashCode()));
+//
+//        String string = str11.replace('f','a');
+//        System.out.println(Integer.toHexString(string.hashCode()));
+//
+//        System.out.println(string);
+
+        String str = "helloworld" ;
+        System.out.println(str.substring(5));
+        System.out.println(str.substring(0, 5));
+
+
+    }
+}
