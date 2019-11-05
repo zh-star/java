aa3510f42501115c0d1ef95587ec6fb82edd73f3
GitHubzhxx <1731866148@qq.com>
2019-11-05 18:58:06 +0800

    git

--- 20191023LinkedList/src/ListNode/ListNode.java
+++ 20191023LinkedList/src/ListNode/ListNode.java
@@ -8,14 +8,36 @@ package ListNode;
  * Time: 22:20
  */
 class Node {
-    private int date;
-    private Node next;
+    public int data;
+    public Node next;
 
-    public Node(int date) {
-        this.date = date;
+    public Node(int data) {
+        this.data = data;
     }
 
 }
 public class ListNode {
-
+    //合并两个单链表
+    public static Node mergeTwoLists(Node head1,Node head2) {
+        Node newHead = new Node(-1);
+        Node newTail = newHead;
+        while(head1 != null && head2 != null) {
+            if(head1.data > head2.data) {
+                newTail.next = head1;
+                newTail = newTail.next;
+                head1 = head1.next;
+            } else {
+                newTail.next = head2;
+                newTail = newTail.next;
+                head2 = head2.next;
+            }
+        }
+        if(head1 == null) {
+            newTail.next = head2;
+        }
+        if(head2 == null) {
+            newTail.next = head1;
+        }
+        return newHead.next;
+    }
 }
