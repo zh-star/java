2aae3a4e51a5de15e913167b374eb8dc73c7118d
GitHubzhxx <1731866148@qq.com>
2019-11-07 17:30:29 +0800

    git

--- 20191115/src/TestDemo3.java
+++ 20191115/src/TestDemo3.java
@@ -10,16 +10,23 @@ import java.util.Scanner;
  */
 public class TestDemo3 {
 
-    public static void main(String[] args) {
+    //翻转字符串 牛客网
+    //输入 ：i am a student  输出：i ma a tneduts
+    public static void main4(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String str = scanner.nextLine();
         char[] ch = str.toCharArray();
+        //System.out.println(Arrays.toString(ch));
         int i = 0;
         int start = 0;
-        while(i < ch.length -1) {
+        while(i < ch.length) {
             if(ch[i] == ' ') {
-                str = reverse(str,start,i);
+                str = reverse(str,start,i -1);
                 start = i + 1;
+                i ++;
+            } else if(i == ch.length - 1){
+                str = reverse(str,start,i -1);
+                i++;
             } else {
                 i ++;
             }
@@ -28,13 +35,27 @@ public class TestDemo3 {
     }
     public static String reverse(String str, int start, int end) {
         char[] ch = str.toCharArray();
+        //System.out.println(Arrays.toString(ch));
         while(start < end) {
             char temp = ch[start];
             ch[start] = ch[end];
             ch[end] = temp;
+            start ++;
+            end --;
         }
         return String.copyValueOf(ch);
     }
+
+
+    //翻转字符串 leetcode
+    //示例 1：输入: "the sky is blue"    输出: "blue is sky the"
+
+    //示例 2：输入: "  hello world!        "输出: "world! hello"
+    //解释: 输入字符串可以在前面或者后面包含多余的空格，但是反转后的字符不能包括。
+
+    //示例 3：输入: "a good   example"    输出: "example good a"
+    //解释: 如果两个单词间有多余的空格，将反转后单词间的空格减少到只含一个。
+
     public static void main2(String[] args){
         String s = "a good   example";
         String[] str = s.split(" ");
@@ -96,6 +117,9 @@ public class TestDemo3 {
         return String.copyValueOf(value);
     }
 
+
+    //字符串拼接
+    //输入 abc def  gh  输出 abcdefgh  注：一行可多输入
     public static void main1(String[] args){
         Scanner scanner = new Scanner(System.in);
         String str = scanner.nextLine();
