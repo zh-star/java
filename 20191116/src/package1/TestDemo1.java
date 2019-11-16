9d49f807b80560a5967442d808c8c40a15ecdb4b
GitHubzhxx <1731866148@qq.com>
2019-11-16 22:05:02 +0800

    git

--- 20191116/src/package1/TestDemo1.java
+++ 20191116/src/package1/TestDemo1.java
@@ -101,6 +101,13 @@ public class TestDemo1 {
         System.out.println(ret);
     }
 
+    //541. 反转字符串 II
+    // 给定一个字符串和一个整数 k，你需要对从字符串开头算起的每个 2k 个字符的前k个字符进行反转。如果剩余少于 k 个字符，则将剩余的所有全部反转。如果有小于 2k 但大于或等于 k 个字符，则反转前 k 个字符，并将剩余的字符保持原样。
+    //示例:
+    //
+    //输入: s = "abcdefg", k = 2
+    //输出: "bacdfeg"
+
     public static String reverseStr(String s, int k) {
         int start = 0;
         for(int i = 0; i < s.length(); i += 2*k) {
