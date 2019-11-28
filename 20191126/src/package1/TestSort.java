33f92d1d45f86a63b5f12ef9d6e09345fa3fe3f4
GitHubzhxx <1731866148@qq.com>
2019-11-28 19:51:18 +0800

    git

--- 20191126/src/package1/TestSort.java
+++ 20191126/src/package1/TestSort.java
@@ -16,7 +16,7 @@ public class TestSort {
         //int[] array = {6,8,2,7,10};
         int[] array = {5,9,12,6,8,34,33,56,89,0,4,7,22,55,77};
        // insertSort(array);
-        quickSort(array,0,array.length -1);
+        mergeSort(array,0,array.length -1);
         //shellSort(array);
         //quickSort(array);
         System.out.println(Arrays.toString(array));
@@ -187,6 +187,42 @@ public class TestSort {
                 stack.push(par+1);
                 stack.push(high);
             }
+        }
+    }
+
+    //归并排序
+    public static void mergeSort(int[] array,int low,int high) {
+        if(low == high) {
+            return;
+        }
+        int mid = (low + high) / 2 ;
+        mergeSort(array,low,mid);
+        mergeSort(array,mid+1,high);
+        merge(array,low,mid,high);
+    }
+    //合并
+    public static void merge(int[] array,int low,int mid,int high) {
+        int s1 = low;
+        int s2 = mid+1;
+        int len = high - low +1; //每一段的长度
+        int[] tmp = new int[len];
+        int i = 0;
+        while(s1 <= mid && s2 <= high) {
+            if(array[s1] <= array[s2]) {
+                tmp[i++] = array[s1++];
+            } else {
+                tmp[i++] = array[s2++];
+            }
+        }
+        while(s1 <= mid) {
+            tmp[i++] = array[s1++];
+        }
+        while(s2 <= high) {
+            tmp[i++] = array[s2++];
+        }
+
+        for (int j = 0; j < tmp.length; j++) {
+            array[low + j] = tmp[j];
         }
     }
 
