9bc8e636ab64b1673f7759b4ed194007cbb86f3a
GitHubzhxx <1731866148@qq.com>
2019-12-25 21:47:27 +0800

    git

--- 每日一提/src/p20191224/p20191224.java
+++ 每日一提/src/p20191224/p20191224.java
@@ -12,12 +12,13 @@ import java.util.Scanner;
 public class p20191224 {
 
     // 1.超长正整数相加
-    public static void main(String[] agrs) {
+    public static void main1(String[] agrs) {
         Scanner sc = new Scanner(System.in);
         String addend = sc.nextLine();
         String augend = sc.nextLine();
         String ret = AddLongInteger(addend,augend);
         System.out.println(ret);
+
     }
     public static String AddLongInteger(String addend,String augend) {
         StringBuffer ret = new StringBuffer();
@@ -48,7 +49,45 @@ public class p20191224 {
             ret.append(temp);
         }
         return ret.reverse().toString();
+
     }
 
-    //2.
+    //2. 跟着奥巴马一起编程
+    // 打印正方形  打印结果是这样
+    // aaaaaaaaaa
+    // a        a
+    // a        a
+    // a        a
+    // aaaaaaaaaa
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()) {
+            int n = sc.nextInt();
+            String str = sc.next();
+            drawSquare(n,str);
+        }
+    }
+    public static void drawSquare(int n, String str) {
+        int list = n; // 列
+        // Math.round(float)返回最接近的参数 int ，其中 int四舍五入为正无穷大。
+        int row = Math.round((float) list / 2); //行
+        for(int i = 0; i < row; i++) {
+            // 打印第一行和最后一行
+            if(i == 0 || i == row - 1) {
+                for(int j = 0; j < list; j ++) {
+                    System.out.print(str);
+                }
+            } else {  // 打印中间行
+                for(int j = 0; j < list; j ++) {
+                    // 每行 第一列 和最后一列 打印字母
+                    if(j == 0 || j == list - 1) {
+                        System.out.print(str);
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+            }
+            System.out.println();
+        }
+    }
 }
---
+++ 每日一提/src/p20191225/p20191225.java
@@ -0,0 +1,80 @@
+package p20191225;
+
+import java.util.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: STAR
+ * Date: 2019 -12
+ * Time: 20:03
+ */
+public class p20191225{
+    // 1. 求一串数字组成的最小数
+    public static void main1(String args[]) {
+        List<Integer> list = new ArrayList<>();
+        int[] arr = new int[10];
+        Scanner sc = new Scanner(System.in);
+        //输入数据
+        int i = 0;
+        for(i = 0;i < 10;i++) {
+            arr[i] = sc.nextInt();
+        }
+        //把从数字1开始的第一个不为0的数字放到list第一个
+        if(arr[0] != 0) {
+            for( i = 1;i < 10;i++) {
+                if(arr[i] != 0){
+                    list.add(i);
+                    arr[i]--;
+                    break;
+                }
+            }
+        }
+        //把剩余数字按位置放入list
+        for(i = 0;i < 10;i++) {
+            for(int j = 0;j < arr[i];j++) {
+                list.add(i);
+            }
+        }
+        //按顺序输出即可
+        for(int t : list) {
+            System.out.print(t);
+        }
+    }
+
+    // 2.求
+    // 2^3 =
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        System.out.println(GetSequeOddNum(m));
+    }
+    public static String GetSequeOddNum(int m) {
+        StringBuffer sb = new StringBuffer();
+        double sum = Math.pow(m,3); // 求立方
+        int avg = (int)sum / m;
+        // 是偶数
+        if(avg%2 == 0) {
+            int temp = avg - (m/2)*2 + 1;
+            for(int i = 0; i < m; i ++) {
+                if(i != m -1) {
+                    sb.append( temp + "+");
+                } else {
+                    sb.append(temp);
+                }
+                temp += 2;
+            }
+        } else {
+            int temp = avg - (m/2)*2;
+            for(int i = 0; i < m; i ++) {
+                if(i != m -1) {
+                    sb.append( temp + "+");
+                } else {
+                    sb.append(temp);
+                }
+                temp += 2;
+            }
+        }
+        return sb.toString();
+    }
+}
