d8551d4f3ae0bff2800375c4cc3cdd8cb819c250
GitHubzhxx <1731866148@qq.com>
2020-01-12 22:58:48 +0800

    git

---
+++ 每日一提/src/p20200110/p20200110.java
@@ -0,0 +1,34 @@
+package p20200110;
+
+import java.util.Scanner;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: STAR
+ * Date: 2020 -01
+ * Time: 22:56
+ */
+public class p20200110 {
+    // 1. 手形数
+    public static void main(String[] args) {
+        Scanner in = new  Scanner(System.in);
+        while (in.hasNextInt()) {
+            int N = in.nextInt();
+            int avg = N*N;
+            while(N > 0) {
+                int temp1 = avg % 10;
+                int temp2 = N % 10;
+                if( temp1 != temp2) {
+                    System.out.println("No!");
+                    break;
+                }
+                avg /= 10;
+                N /= 10;
+            }
+            if(N <= 0) {
+                System.out.println("Yes!");
+            }
+        }
+    }
+}
