51bd5ed20c3ed296c29d2a2542aa9216d9c8047e
GitHubzhxx <1731866148@qq.com>
2020-01-12 23:01:14 +0800

    git

--- 每日一提/src/p20200110/p20200110.java
+++ 每日一提/src/p20200110/p20200110.java
@@ -11,6 +11,7 @@ import java.util.Scanner;
  */
 public class p20200110 {
     // 1. 手形数
+    // 一个树的平方后几位数如果等与这个数 就叫这个数是手形数
     public static void main(String[] args) {
         Scanner in = new  Scanner(System.in);
         while (in.hasNextInt()) {
