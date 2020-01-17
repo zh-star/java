fe50e06e6f42f372c553bf2abd691f2e1d008b33
GitHubzhxx <1731866148@qq.com>
2020-01-17 22:23:25 +0800

    git

--- 每日一提/src/p2020_01/P20200116/P20200116.java
+++ 每日一提/src/p2020_01/P20200116/P20200116.java
@@ -32,6 +32,7 @@ public class P20200116 {
 //        //int result = 0;
         return (countWays(n-1) + countWays(n-2) + countWays(n-3))%1000000007;
     }
+
     public static int countWays(int n) {
         if(n == 1) {
             return 1;
